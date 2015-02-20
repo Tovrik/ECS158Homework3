28e75c59279a4c0700c71e9294e07c051790cd82
Stefan Peterson <stefan.t.peterson@gmail.com>
2015-02-20 02:42:14 -0800

    added the two files

---
+++ SmoothC.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+void smoothc(float *x, float *y, float *m, int n, float h) {
+
+}
+
+int main (int argc, char** argv) {
+	// rmandel(nth, xl, xr, yb, yt, inc, maxiters, sched, chunksize);
+	return 0;
+}
