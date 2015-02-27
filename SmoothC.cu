8148efce594cb5618213f93a159f07bef989227e
Bijan S Agahi <bagahi@pc33.cs.ucdavis.edu>
2015-02-26 16:37:22 -0800

    Go fuck yourself Stefan. You and your damn tic tac dicks

--- SmoothC.cu
+++ SmoothC.cu
@@ -13,13 +13,13 @@ __global__ void findY(float *x, float *y, int n, float h, float z, int zLoc, flo
 	// int row = blockIdx.y * blockDim.y + threadIdx.y;
 
 	__shared__ float sum;
-	sum = 0;
+	//sum = 0;
 	float absVal = 0;
 	int count = 0;
 	for(int i = 0; i < n; i++) {
 		absVal = abs(x[i] - z);
 		if(absVal < h) {
-			atomicAdd(&sum, y[zLoc]);
+			//sum = atomicAdd(&sum, y[zLoc]);
 		 	sum += y[zLoc];
 		 	count++;
 		}
@@ -41,10 +41,16 @@ void smoothc(float *x, float *y, float *m, int n, float h) {
 
 	float *xChunk;
 	float *yChunk;
+	float *mdev;
 	//min size of x is 512 bytes or 64 entries
 	int msize = chunksize * sizeof(float);
-	cudaMalloc((void **) &xChunk, msize);
-	cudaMalloc((void **) &yChunk, msize);
+	//printf("Got This far!\n");
+	cudaMalloc((void **) &xChunk, 80);
+	cudaMalloc((void **) &yChunk, 80);
+	cudaMalloc((void **) &mdev, 80);
+	printf("Got past the Allocs!\n");
+	// cudaError_t err2 = cudaGetLastError();
+	// if(err2 != cudaSuccess) printf("%s\n", cudaGetErrorString(err2));
 	// dst 		- Destination memory address
 	// src 		- Source memory address
 	// count 	- Size in bytes to copy
@@ -54,13 +60,15 @@ void smoothc(float *x, float *y, float *m, int n, float h) {
 	
 
 	for(int i = 0; i < n; i++) {
-		cudaMemcpy(xChunk, x, msize, cudaMemcpyHostToDevice);
-		cudaMemcpy(yChunk, y, msize, cudaMemcpyHostToDevice);
-		findY<<<dimGrid, dimBlock>>>(xChunk, yChunk, chunksize, h, x[i], i, &m[i]);
+		cout<<"got this far? ("<<i<<")"<<endl;
+		cudaMemcpy(xChunk, x, 80, cudaMemcpyHostToDevice);
+		cudaMemcpy(yChunk, y, 80, cudaMemcpyHostToDevice);
+		findY<<<dimGrid, dimBlock>>>(xChunk, yChunk, chunksize, h, x[i], i, &mdev[i]);
 	}
-
+	cudaMemcpy(m, mdev, 80, cudaMemcpyDeviceToHost);
 	cudaFree(xChunk);
 	cudaFree(yChunk);
+	cudaFree(mdev);
 
 }
 
@@ -70,7 +78,10 @@ int main (int argc, char** argv) {
 	float x[10] = {1,2,3,4,5,6,7,8,9,10};
 	float y[10] = {11,12,13,14,15,16,17,18,19,20};
 	float m[10];
-	smoothc(x,y,m,10,3);
+	for(int i = 0; i < 10; i++)
+		cout << m[i] << endl;
+	smoothc(x,y,m,10,1);
+	cout<<"**********RETURN VALUES:***********"<<endl;
 	for(int i = 0; i < 10; i++)
 		cout << m[i] << endl;
 	return 0;
