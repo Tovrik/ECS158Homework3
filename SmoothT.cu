3d990ff16393ef89c089be7f32791eafd5fa8cec
Arjun <arjunb090@gmail.com>
2015-03-02 15:37:17 -0800

    Stefan pull it

--- SmoothT.cu
+++ SmoothT.cu
@@ -13,7 +13,11 @@ void smootht(float *x, float *y, float *m, int n, float h) {
 }
 
 int main (int argc, char** argv) {
+  cudaDeviceProp Props;
+  cudaGetDeviceProperties(&Props , 0);
+
   int n = 10;
+  float h = 2;
   // Host memory vectors
   thrust:: host_vector<float> x(10);
   thrust:: host_vector<float> y(10);
@@ -32,5 +36,24 @@ int main (int argc, char** argv) {
   thrust::copy(y.begin(), y.end(), std::ostream_iterator<float>(std::cout, "\n"));
 
   // Alocate vector on device
+  thrust:: device_vector<float> xchunk = x;
+  thrust:: device_vector<float> ychunk = y;
+  thrust:: device_vector<float> averageChunk = averageArrays;
+
+  float* xPointer = thrust::raw_pointer_cast( &xchunk[0] );
+  float* yPointer = thrust::raw_pointer_cast( &ychunk[0] );
+  float* avgPointer = thrust::raw_pointer_cast( &averageChunk[0] );
+
+  // Invoke the kernel
+  smootht <<< 1, 1 >>> (xPointer, yPointer, avgPointer, n, h);
+
+  thrust:: copy(averageChunk.begin(), averageChunk.end(), averageArrays.begin());
+
+  for(int i = 0; i < averageChunk.size(); i++) {
+    cout << averageChunk[i] << endl;
+  } 
+
+
+
 
 }
