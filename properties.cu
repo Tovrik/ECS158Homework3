844f50c53d62b87ce7f1214634fd3151f8939c21
Arjun <arjunb090@gmail.com>
2015-03-01 16:15:38 -0800

    add properties file

---
+++ properties.cu
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <iostream>
+#include <cmath>
+#include <string.h>
+using namespace std;
+
+void DisplayHeader()
+{
+   cudaDeviceProp Props ;
+   cudaGetDeviceProperties(&Props , 0) ;
+   printf("shared mem: %d\n", Props.sharedMemPerBlock);
+   printf("max threads: %d\n", Props.maxThreadsPerBlock);
+   printf("max blocks: %d\n", Props.maxGridSize[0]);
+   printf("total Const mem: %d\n", Props.totalConstMem);
+   printf("Major number: %d\n", Props.major);
+   printf("Minor number: %d\n", Props.minor);
+}
+
+int main(int argc, char** argv) {
+  DisplayHeader();
+
+
+}
