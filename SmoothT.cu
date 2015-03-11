4bac597aadf47e5184ffeac08a483138760bc3cb
arjunb090 <arjunb090@gmail.com>
2015-03-11 15:26:16 -0700

    Done. Thrust Works this time.
    PURE THRUST!

--- SmoothT.cu
+++ SmoothT.cu
@@ -11,6 +11,8 @@
 #include <thrust/sequence.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/reduce.h>
+#include <cuda.h>
+
 using namespace std;
 
 typedef thrust::tuple<int, int>            tpl2int;
@@ -19,6 +21,8 @@ typedef thrust::counting_iterator<int>     countiter;
 typedef thrust::tuple<intiter, countiter>  tpl2intiter;
 typedef thrust::zip_iterator<tpl2intiter>  idxzip;
 
+// This functor returns true or false (1 or 0) based on 
+// whether the abs(x1 - x2) is true or false 
 struct finddiff {
   const float a;
   const float x2;
@@ -31,6 +35,7 @@ struct finddiff {
   }
 };
 
+
 struct select_unary_op : public thrust::unary_function<tpl2int, int>
 {
   
@@ -40,69 +45,54 @@ struct select_unary_op : public thrust::unary_function<tpl2int, int>
 
   __device__ int operator()(const tpl2int& x) const
   {
+    // If an element is true, then you get the y coordinate of the position
     if (x.get<0>() == 1)
       return yptr[x.get<1>()];
     else return 0;
    }
 };
 
-struct greater_than_negative_one {
-  __device__
-  bool operator()(int x) {
-    return x != -1;
-  }
-};
-
-
 void smootht(float *x, float *y, float *m, int n, float h) {
-  // Alocate vector on device
+  // Alocate vectors on device
   thrust:: device_vector<float> xchunk(x, x+n);
   thrust:: device_vector<float> ychunk(y, y+n);
   thrust:: device_vector<float> averageChunk(n);
-  thrust:: device_vector<int> A(n);
-  thrust:: sequence(A.begin(), A.end());
+
+  // You use these iterators with the zip iterators
+  // to create a tuple
   thrust::counting_iterator<int> idxfirst(0);
   thrust::counting_iterator<int> idxlast = idxfirst + n;
+
   thrust:: device_vector<int> y_positions(n);
   thrust:: device_vector<int> dv(n);
 
   for(int i = 0; i < n; i++) {
-  
+    // Mark 1 in the array if abs(x1 - x2) is true, else 0
     thrust::transform(xchunk.begin(), xchunk.end(), dv.begin(), finddiff(h, xchunk[i]));
-
-    // cout << "differences: " << endl;
-    // thrust::copy(dv.begin(), dv.end(), std::ostream_iterator<float>(std::cout, "\n"));
-    //cout << "xchunk[i] = " << xchunk[i] << endl;
-    // float sum = 0;
-    // // Count the number of 1s
+    // Count the number of 1's (times the condition was satisfied)
     int count = thrust::count(dv.begin(), dv.end(), 1);
-    //cout << "Count = " << count << endl;
 
-    // // Go through the differences vector and find all elements of 1
-    // // And add those elements
-    // for(int j = 0; j < dv.size(); j++) {
-    //   if(dv[j] == 1) {
-    //     sum = sum + ychunk[j];
-    //   }
-    // }
-    // averageChunk[i] = sum / count;
+    // Use a zip-iterator to make a tuple which has the following format: 
+    // [position of element (1 or 0), index of element]
     idxzip first = thrust::make_zip_iterator(thrust::make_tuple(dv.begin(), idxfirst));
     idxzip last = thrust::make_zip_iterator(thrust::make_tuple(dv.end(), idxlast));
     thrust:: device_ptr<float> yPointer = &ychunk[0];
+
+    // Pass the entire y_positions to my_unary_op 
+    // and find the y values if the value is true, else put a 0
     select_unary_op my_unary_op(yPointer);
     thrust::transform(first, last, y_positions.begin(), my_unary_op);
+
+    // Add all the values up to get the sum of y's
+    // Don't have to worry about other positions because they hold a 0
     float sum = thrust::reduce(y_positions.begin(), y_positions.end());
-    // std::cout << "Results :" << std::endl;
-    // thrust::copy(y_positions.begin(), y_positions.end(), std::ostream_iterator<int>( std::cout, " \n"));
-    // cout << "Average = " << sum / count << endl;
+
+    // Divide the sum by the count to find the average
+    averageChunk[i] = sum / count;
   }
-  //cout << "averageChunk: " << endl;
-  //thrust::copy(averageChunk.begin(), averageChunk.end(), std::ostream_iterator<float>(std::cout, "\n"));
   // Copy from device back to host
   thrust:: copy(averageChunk.begin(), averageChunk.end(), m);
 }
-
-
 
 
 int main(int argc, char** argv) {
@@ -113,16 +103,29 @@ int main(int argc, char** argv) {
   float* x = new float[n];
   float* y = new float[n];
   float* averageArrays = new float[n]; 
-  //memset(averageArrays, 0, sizeof(averageArrays));
   for(int i = 0, j = n; i < n; i++, j++) {
     x[i] = i + 1;
     y[i] = j + 1;
   }
+
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  cudaEventRecord(start, 0);
+
+
   smootht(x, y, averageArrays, n, h);
 
-  // for(int i = 0; i < n; i++) {
-  //    cout << averageArrays[i] << "\n";
-  // }
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  float elapsedTime; 
+  cudaEventElapsedTime(&elapsedTime , start, stop);
+
+  for(int i = 0; i < n; i++) {
+     cout << averageArrays[i] << "\n";
+  }
+
+  printf("Avg. time is %f ms\n", elapsedTime/100);
 
   delete[] averageArrays;
   delete[] y;
